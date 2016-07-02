f853ebfad47a40c8150cc306f43c3f15b46fefc8
fabm <francisco.ab.monteiro@gmail.com>
2016-07-02 09:30:46 +0100

    add root servlet holder

--- src/main/groovy/pt/francisco/server/Jeje.groovy
+++ src/main/groovy/pt/francisco/server/Jeje.groovy
@@ -5,8 +5,16 @@ import groovy.servlet.TemplateServlet
 import org.mortbay.jetty.Server
 import org.mortbay.jetty.servlet.Context
 import org.mortbay.jetty.servlet.DefaultServlet
+import org.mortbay.jetty.servlet.ServletHolder
 import org.mortbay.servlet.MultiPartFilter
 
+import javax.servlet.Servlet
+import javax.servlet.ServletException
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 import java.nio.file.Paths
 
 new Server(9090).with {
@@ -18,9 +26,16 @@ new Server(9090).with {
         }
         resourceBase = properties.get('serverResources')
 
-        print getResource('/static/index.html').file.absolutePath
-
         addFilter MultiPartFilter, '/*', DEFAULT
+
+        addServlet new ServletHolder(new HttpServlet(){
+            @Override
+            void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+                assert res instanceof HttpServletResponse
+                res.sendRedirect '/static/generators.html'
+            }
+        }), '/*'
+
         addServlet(DefaultServlet, '/static/*').with {
             setInitParameter 'dirAllowed', 'true'
         }
